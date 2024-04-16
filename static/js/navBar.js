ff5b03fcd746a7201ba013972b9f2f11ac8f40bd
Andra <111245270+Andra-sun@users.noreply.github.com>
2024-04-16 17:10:59 -0300

    Add files via upload

--- static/js/navBar.js
+++ static/js/navBar.js
@@ -10,3 +10,36 @@ function showBreeds(species) {
         breedListCat.style.display = catCheckbox.checked ? "block" : "none";
     }
 }
+
+function toggleListVisibility(listId, spanId) {
+    var list = document.getElementById(listId);
+    var span = document.getElementById(spanId);
+    list.classList.toggle("hidden");
+    if (!list.classList.contains("hidden")) {
+        span.innerHTML = "&#x25B4;";
+    } else {
+        span.innerHTML = "&#x25BE;";
+    }
+}
+
+document.getElementById("speciesTitle").addEventListener("click", function () {
+    toggleListVisibility("speciesList", "speciesTitleArrow");
+});
+
+document.getElementById("sizeTitle").addEventListener("click", function () {
+    toggleListVisibility("sizeList", "sizeTitleArrow");
+});
+
+document.getElementById("ageTitle").addEventListener("click", function () {
+    toggleListVisibility("ageList", "ageTitleArrow");
+});
+
+document
+    .getElementById("deficiencyTitle")
+    .addEventListener("click", function () {
+        toggleListVisibility("deficiencyList", "deficiencyTitleArrow");
+    });
+
+document.getElementById("genderTitle").addEventListener("click", function () {
+    toggleListVisibility("genderList", "genderTitleArrow");
+});
